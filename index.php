c2e1737b684d828cd796753b17b382ca4342316c
lichblitz <acid.storm3@gmail.com>
2014-08-17 22:58:33 -0500

    Aplicacion Android
    Creado proyecto en Android Studio

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
-<?
-require 'Slim/Slim.php';
-\Slim\Slim::registerAutoloader();
+<?php
+	require 'Slim/Slim.php';
+	\Slim\Slim::registerAutoloader();
 
+	//With custom settings
+	$app = new \Slim\Slim(array(
+	    'log.enable' => true,
+	    'log.path' => './logs',
+	    'log.level' => 4
+	));
+
+	$app->get('/', function() use ($app){
+		require_once 'lib/mysql.php';
+		$db = connect_db();
+	});
+	
+	$app->run();
 ?>
