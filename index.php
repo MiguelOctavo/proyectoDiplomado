4686dcb776d907448b3679e6550ef576bd3d0663
lichblitz <acid.storm3@gmail.com>
2014-08-17 22:59:24 -0500

    Revert "Aplicacion Android"
    This reverts commit c2e1737b684d828cd796753b17b382ca4342316c.

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
-<?php
-	require 'Slim/Slim.php';
-	\Slim\Slim::registerAutoloader();
+<?
+require 'Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
 
-	//With custom settings
-	$app = new \Slim\Slim(array(
-	    'log.enable' => true,
-	    'log.path' => './logs',
-	    'log.level' => 4
-	));
-
-	$app->get('/', function() use ($app){
-		require_once 'lib/mysql.php';
-		$db = connect_db();
-	});
-	
-	$app->run();
 ?>
